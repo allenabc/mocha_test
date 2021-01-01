55b9222209ccae38a8cdffdc01f2d221cc83a5c5
allenabc <allen_abc@yahoo.com>
2020-12-31 21:56:49 -0700

    reformat tests

--- test/assertStylesExample.test.js
+++ test/assertStylesExample.test.js
@@ -1,31 +1,29 @@
 // Assert style:
 
-console.log("Running assert style asserts")
-var assert = require('chai').assert;
-var numbers = [1, 2, 3, 4, 5];
+console.log('Running assert style asserts')
+var assert = require('chai').assert
+var numbers = [1, 2, 3, 4, 5]
 
-assert.isArray(numbers, 'is array of numbers');
-assert.include(numbers, 2, 'array contains 2');
-assert.lengthOf(numbers, 5, 'array contains 5 numbers');
-
+assert.isArray(numbers, 'is array of numbers')
+assert.include(numbers, 2, 'array contains 2')
+assert.lengthOf(numbers, 5, 'array contains 5 numbers')
 
 // Expect style:
 
-console.log("Running Expect style asserts")
-var expect = require('chai').expect;
-var numbers = [1, 2, 3, 4, 5];
+console.log('Running Expect style asserts')
+var expect = require('chai').expect
+var numbers = [1, 2, 3, 4, 5]
 
-expect(numbers).to.be.an('array').that.includes(2);
-expect(numbers).to.have.lengthOf(5);
+expect(numbers).to.be.an('array').that.includes(2)
+expect(numbers).to.have.lengthOf(5)
 
 // Should style:
-console.log("Running Should style asserts")
+console.log('Running Should style asserts')
 
-var should = require('chai').should();
-var numbers = [1, 2, 3, 4, 5];
+var should = require('chai').should()
+var numbers = [1, 2, 3, 4, 5]
 
-numbers.should.be.an('array').that.includes(5);
-numbers.should.have.lengthOf(5);
-
+numbers.should.be.an('array').that.includes(5)
+numbers.should.have.lengthOf(5)
 
 // from https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/
--- test/math.test.js
+++ test/math.test.js
@@ -1,22 +1,22 @@
-var assert = require('assert');
+var assert = require('assert')
 // Create a test suite (group) called Math
-describe('Arithmetic', function() {
-    // Test One: What we're testing
-    it('should test if 6 + 7  = 13', function(){
-      assert.equal(6+7, 13);
-    });
-    // Test Two: A string explanation of what we're testing
-    it('should test if 6 * 7 = 42', function(){
-      // Test:  6 * 7  should equal 42
-      assert.equal(6*7, 42);
-    });
-    // Test Three: What we're testing
-    it('should test if 6 - 7  = -1', function(){
-      assert.equal(6-7, -1);
-    });
-    // Test Four: A string explanation of what we're testing
-    it('should test if 6 / 7 = .9', function(){
-      // Test:  6 / 7  should equal .9
-      assert.equal((6/7).toFixed(1), .9);
-    });
-});
+describe('Arithmetic', function () {
+  // Test One: What we're testing
+  it('should test if 6 + 7  = 13', function () {
+    assert.equal(6 + 7, 13)
+  })
+  // Test Two: A string explanation of what we're testing
+  it('should test if 6 * 7 = 42', function () {
+    // Test:  6 * 7  should equal 42
+    assert.equal(6 * 7, 42)
+  })
+  // Test Three: What we're testing
+  it('should test if 6 - 7  = -1', function () {
+    assert.equal(6 - 7, -1)
+  })
+  // Test Four: A string explanation of what we're testing
+  it('should test if 6 / 7 = .9', function () {
+    // Test:  6 / 7  should equal .9
+    assert.equal((6 / 7).toFixed(1), 0.9)
+  })
+})
